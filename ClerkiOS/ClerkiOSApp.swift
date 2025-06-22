eda0768d45cb3f12c93664f46e299f645500cae0
AlexX <alex98777@icloud.com>
2025-06-22 08:20:21 -0600

    Initial Commit

---
+++ ClerkiOS/ClerkiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClerkiOSApp.swift
+//  ClerkiOS
+//
+//  Created by Alex on 2025-06-22.
+//
+
+import SwiftUI
+
+@main
+struct ClerkiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
